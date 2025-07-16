20350c4705f67bdfe204cd7f098f22fc22b839fb
MarceloHaa <marcelohenrique.a@hotmail.com>
2025-07-16 17:13:05 -0300

    Class 5 Updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app2_series/tv_show_card.dart';
+import 'package:flutter_app2_series/add_tv_show_screen.dart';
+import 'package:flutter_app2_series/custom_drawer.dart';
 import 'package:flutter_app2_series/tv_show_data.dart';
+import 'package:flutter_app2_series/tv_show_model.dart';
+import 'package:flutter_app2_series/tv_show_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  final List<TvShow> tvShows = favTvShowList;
+
+  int currentScreenIndex = 0;
+
+  void addTvShow(TvShow newTvShow) {
+    setState(() {
+      tvShows.add(newTvShow);
+    });
+  }
+
+  List<Widget> get screens => [
+    TvShowScreen(tvShows: tvShows),
+    AddTvShowScreen(onAddTvShow: addTvShow),
+  ];
+
+  void switchScreen(int index) {
+    setState(() {
+      currentScreenIndex = index;
+    });
+  }
+
+  bool isDark = false;
+  void switchTheme() {
+    setState(() {
+      isDark = !isDark;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,6 +72,7 @@ class MainApp extends StatelessWidget {
           fontWeight: FontWeight.bold,
           color: colorScheme.onPrimary,
         ),
+        iconTheme: IconThemeData(color: colorScheme.onPrimary, size: 36),
       ),
       cardTheme: CardThemeData(
         color: colorScheme.secondaryContainer,
@@ -58,6 +95,7 @@ class MainApp extends StatelessWidget {
           fontWeight: FontWeight.bold,
           color: colorSchemeDark.primary,
         ),
+        iconTheme: IconThemeData(color: colorScheme.onPrimary, size: 36),
       ),
       cardTheme: CardThemeData(
         color: colorSchemeDark.secondaryContainer,
@@ -72,21 +110,20 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: customTheme,
       darkTheme: customThemeDark,
-      themeMode: ThemeMode.system,
+      themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
       home: Scaffold(
-        appBar: AppBar(title: const Text('Eu Amo Séries 🎬')),
-        // body: ListView(
-        //   children: [
-        //     ...favTvShowList.map(
-        //       (tvShow) => TvShowCard(tvShow: tvShow)
-        //     ),
-        //   ],
-        // ),
-        body: ListView.builder(
-          itemCount: favTvShowList.length,
-          itemBuilder: (context, index) =>
-              TvShowCard(tvShow: favTvShowList[index], index: index),
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [const Text('Eu Amo Séries 🎬')],
+          ),
         ),
+        drawer: CustomDrawer(
+          isDark: isDark,
+          switchTheme: switchTheme,
+          switchScreen: switchScreen,
+        ),
+        body: screens[currentScreenIndex],
       ),
     );
   }
